c4e5ae66b1b589e22332a70260909365604a52ff
avillaca <ari.villaca@milliman.com>
2022-03-15 20:31:48 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("97ba9b61-44e6-4c26-89fa-c4916f89df2a");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
